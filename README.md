8c579e2e7d30ccf60febd4354ddc5c1db8ec03cf
Chandan <chandan743247@gmail.com>
2025-02-22 13:09:17 +0530

    Scientific layout is complited

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Advance-GUI-based-Scientific-Calculator
+
+Create Calculator app using Python and tkinter that can switch basic and scientific mode
+
+![Calculator App](/resource/Calculator.webm)
--- calculator.py
+++ calculator.py
@@ -25,7 +25,13 @@ class Calculator:
             7: (3, 1), 8: (3, 2), 9: (3, 3),
             4: (4, 1), 5: (4, 2), 6: (4, 3),
             1: (5, 1), 2: (5, 2), 3: (5, 3),
-            0: (6, 2), '.': (6, 1)
+            0: (6, 2), '.': (6, 3)
+        }
+        self.operator = {
+            '/' : ('/', 2, 4),
+            '*' : ('*', 3, 4),
+            '-' : ('-', 4, 4),
+            '+' : ('+', 5, 4)
         }
 
         self.display_frame = self.create_display_frame()
@@ -53,12 +59,60 @@ class Calculator:
         cur_level.pack(expand=True, fill='both')
 
         return total_level, cur_level
+
+    def toggle(self):
+        self.is_scientific = not self.is_scientific
+        self.create_display_buttons()
     
     def create_display_buttons(self):
+        self.clear_buttons()
+        self.buttons_frame = self.create_buttons_frame()
         if self.is_scientific:
+            self.create_2nd_button()
+            self.create_deg_button()
+            self.create_sin_button()
+            self.create_cos_button()
+            self.create_tan_button()
+            self.create_power_button()
+            self.create_log_button()
+            self.create_ln_button()
+            self.create_open_parenthesis_button()
+            self.create_close_parenthesis_button()
+
             self.create_scientific_buttons()
+            self.create_operator_buttons()
+            self.create_equals_button()
+            self.create_clear_button()
+            self.create_backspace_button()
+            self.create_percentage_button()
+
+            self.create_sqrt_button()
+            self.create_factorial_button()
+            self.create_inverse_button()
+            self.create_pie_button()
+            self.create_e_button()
+            self.create_toggle_button(6, 0)
+
+            for r in range(7):
+                self.buttons_frame.rowconfigure(r, weight=1)
+
+            for c in range(5):
+                self.buttons_frame.columnconfigure(c, weight=1)
+
         else:
             self.create_basic_buttons()
+            self.create_operator_buttons()
+            self.create_equals_button()
+            self.create_clear_button()
+            self.create_backspace_button()
+            self.create_percentage_button()
+            self.create_toggle_button(6, 1)
+
+            for r in range(2, 7):
+                self.buttons_frame.rowconfigure(r, weight=1)
+
+            for c in range(1, 5):
+                self.buttons_frame.columnconfigure(c, weight=1)
 
     def create_basic_buttons(self):
         
@@ -70,6 +124,96 @@ class Calculator:
         for key, pos in self.digits.items():
             button = tk.Button(self.buttons_frame, bg=WHITE, fg=LABEL_COLOR, text=str(key), font=DIGITS_FONT_STYLE, borderwidth=0)
             button.grid(row=pos[0], column=pos[1], sticky=tk.NSEW)
+    
+    def create_operator_buttons(self):
+        for key, pos in self.operator.items():
+            button = tk.Button(self.buttons_frame, text=pos[0], bg=OFF_WHITE, font=DIGITS_FONT_STYLE, borderwidth=0)
+            button.grid(row=pos[1], column=pos[2], sticky=tk.NSEW)
+    
+    def create_equals_button(self):
+        button = tk.Button(self.buttons_frame, text='=', bg=LIGHT_BLUE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=6, column=4, sticky=tk.NSEW)
+
+    def create_percentage_button(self):
+        button = tk.Button(self.buttons_frame, text='%', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=2, column=3, sticky=tk.NSEW)
+
+    def create_backspace_button(self):
+        button = tk.Button(self.buttons_frame, text='\u232B', bg=OFF_WHITE, fg=LABEL_COLOR, font=SMALL_FONT_STYLE, borderwidth=0)
+        button.grid(row=2, column=2, sticky=tk.NSEW)
+
+    def create_clear_button(self):
+        button = tk.Button(self.buttons_frame, text='C', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=2, column=1, sticky=tk.NSEW)
+    
+    def create_toggle_button(self, r, c):
+        button = tk.Button(self.buttons_frame, text='T', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0, command=self.toggle)
+        button.grid(row=r, column=c, sticky=tk.NSEW)
+
+    def create_2nd_button(self):
+        button = tk.Button(self.buttons_frame, text='2nd', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=0, column=0, sticky=tk.NSEW)
+    
+    def create_deg_button(self):
+        button = tk.Button(self.buttons_frame, text='deg', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=0, column=1, sticky=tk.NSEW)
+
+    def create_sin_button(self):
+        button = tk.Button(self.buttons_frame, text='sin', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=0, column=2, sticky=tk.NSEW)
+    
+    def create_cos_button(self):
+        button = tk.Button(self.buttons_frame, text='cos', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=0, column=3, sticky=tk.NSEW)
+
+    def create_tan_button(self):
+        button = tk.Button(self.buttons_frame, text='tan', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=0, column=4, sticky=tk.NSEW)
+    
+    def create_power_button(self):
+        button = tk.Button(self.buttons_frame, text='X\u02b8', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=1, column=0, sticky=tk.NSEW)
+
+    def create_log_button(self):
+        button = tk.Button(self.buttons_frame, text='lg', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=1, column=1, sticky=tk.NSEW)
+    
+    def create_ln_button(self):
+        button = tk.Button(self.buttons_frame, text='ln', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=1, column=2, sticky=tk.NSEW)
+
+    def create_open_parenthesis_button(self):
+        button = tk.Button(self.buttons_frame, text='(', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=1, column=3, sticky=tk.NSEW)
+    
+    def create_close_parenthesis_button(self):
+        button = tk.Button(self.buttons_frame, text=')', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=1, column=4, sticky=tk.NSEW)
+
+    def create_sqrt_button(self):
+        button = tk.Button(self.buttons_frame, text='\u221Ax', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=2, column=0, sticky=tk.NSEW)
+    
+    def create_factorial_button(self):
+        button = tk.Button(self.buttons_frame, text='x!', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=3, column=0, sticky=tk.NSEW)
+    
+    def create_inverse_button(self):
+        button = tk.Button(self.buttons_frame, text='1/x', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=4, column=0, sticky=tk.NSEW)
+    
+    def create_pie_button(self):
+        button = tk.Button(self.buttons_frame, text='\u03c0', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=5, column=0, sticky=tk.NSEW)
+
+    def create_e_button(self):
+        button = tk.Button(self.buttons_frame, text='e', bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button.grid(row=6, column=1, sticky=tk.NSEW)
+
+    def clear_buttons(self):
+        self.buttons_frame.destroy()
+        # for button in self.buttons_frame.winfo_children():
+        #     button.destroy()
 
     def run(self):
         self.root.mainloop()
